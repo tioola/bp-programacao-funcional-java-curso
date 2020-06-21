106bbfc241b98717d79304bd238fc7c682c7e93e
Diogo Fabrile <diogo@asaitec.eu>
2020-06-21 14:22:08 +0200

    Alteracoes modulo 02

---
+++ src/test/java/modulo02/_02_01_ImperativoVsDeclarativo.java
@@ -0,0 +1,13 @@
+package modulo02;
+
+public class _02_01_ImperativoVsDeclarativo {
+
+    // TODO 01 - Buscar Product através do nome de forma imperativa
+
+   // TODO 02 - Buscar Product através do nome de forma declarativa
+
+   // TODO 03 - Calcular total de preços dos produtos listados de forma imperativa
+
+   // TODO 04 - Calcular total de preços dos produtos listados de forma declarativa
+
+}
